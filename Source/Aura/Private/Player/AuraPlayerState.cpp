bf7adf9c5cd91eeeb6fce8c621ab1089c33fa604
avakad0 <36058984+avakad0@users.noreply.github.com>
2023-10-24 16:18:37 -0700

    Replaction Mode

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -2,13 +2,14 @@
 
 
 #include "Player/AuraPlayerState.h"
-#include <AbilitySystem/AuraAbilitySystemComponent.h>
-#include <AbilitySystem/AuraAttributeSet.h>
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
 
 AAuraPlayerState::AAuraPlayerState()
 {
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
 	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
 
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 
